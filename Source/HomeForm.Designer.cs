f4e82bac98d3e0ad2d029d39e4bef42fb24d6370
Sheng Kai <maimemdada@gmail.com>
2022-12-12 15:28:31 +0700

    add panel

--- Source/HomeForm.Designer.cs
+++ Source/HomeForm.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnPlay = new System.Windows.Forms.Panel();
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.pnMain = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // pnPlay
+            // 
+            this.pnPlay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.pnPlay.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnPlay.Location = new System.Drawing.Point(0, 452);
+            this.pnPlay.Name = "pnPlay";
+            this.pnPlay.Size = new System.Drawing.Size(1004, 135);
+            this.pnPlay.TabIndex = 0;
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.pnMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(272, 452);
+            this.pnMenu.TabIndex = 1;
+            // 
+            // pnMain
+            // 
+            this.pnMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(24)))), ((int)(((byte)(24)))));
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(272, 0);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(732, 452);
+            this.pnMain.TabIndex = 2;
             // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(817, 474);
+            this.ClientSize = new System.Drawing.Size(1004, 587);
+            this.Controls.Add(this.pnMain);
+            this.Controls.Add(this.pnMenu);
+            this.Controls.Add(this.pnPlay);
             this.Name = "HomeForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MusicApp";
@@ -43,5 +76,9 @@
         }
 
         #endregion
+
+        private Panel pnPlay;
+        private Panel pnMenu;
+        private Panel pnMain;
     }
 }
