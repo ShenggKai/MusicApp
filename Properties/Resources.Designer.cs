e6ed727ec66b72d3c462165ac8768a687b061c40
Sheng Kai <maimemdada@gmail.com>
2022-12-19 09:52:29 +0700

    add color to find but

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -273,6 +273,16 @@ namespace MusicApp.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap search_black {
+            get {
+                object obj = ResourceManager.GetObject("search_black", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap search_white {
             get {
                 object obj = ResourceManager.GetObject("search_white", resourceCulture);
