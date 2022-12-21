5e813d98d69b15d025f8e2ee52fa7aaf310df166
Sheng Kai <maimemdada@gmail.com>
2022-12-21 09:12:19 +0700

    done with add code to user control

--- Source/Song.cs
+++ Source/Song.cs
@@ -16,5 +16,41 @@ namespace MusicApp.Source
         {
             InitializeComponent();
         }
+
+        public Image aImageSong
+        {
+            get
+            {
+                return pSong.Image;
+            }
+            set
+            {
+                pSong.Image = value;
+            }
+        }
+
+        public String aNameSong
+        {
+            get { return txtName.Text; }
+            set { txtName.Text = value; }
+        }
+
+        public String aArtist
+        {
+            get { return txtArtist.Text; }
+            set { txtArtist.Text = value; }
+        }
+
+        public String aNumber
+        {
+            get { return txtNum.Text; }
+            set { txtNum.Text = value; }
+        }
+
+        public String aTime
+        { 
+            get { return txtTime.Text; }
+            set { txtTime.Text = value; }
+        }
     }
 }
